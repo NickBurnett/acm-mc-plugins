29a108364c5db2f2fcc30dab36b4e76384b9de00
Nicolas Burnett <nicolasandrewburnett@gmail.com>
2021-03-19 21:42:13 -0500

    Dependency Fix

--- CORE/build.gradle.kts
+++ CORE/build.gradle.kts
@@ -7,16 +7,14 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-    }
+    jcenter()
     maven {
         url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     }
-    mavenLocal()
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot:1.16.5-R0.1-SNAPSHOT")
-    implementation(project(":API"))
+    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+
 }
