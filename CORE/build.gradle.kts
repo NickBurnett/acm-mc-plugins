89cba57e6b64382d9a1625d4aaa8c74c7b90b217
Nicolas Burnett <nicolasandrewburnett@gmail.com>
2021-03-19 21:46:41 -0500

    Dependency Fix

--- CORE/build.gradle.kts
+++ CORE/build.gradle.kts
@@ -16,5 +16,5 @@ repositories {
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
-
+    implementation(project(":API"))
 }
